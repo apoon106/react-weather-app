79bc9596f961b0e59b5fc62d97a1e10caab228c8
apoon106 <121922430+apoon106@users.noreply.github.com>
2023-04-25 21:25:42 -0400

    Added rel="nonreferrer"  on link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ export default function App() {
       Weather App
       <footer>
         This project was coded by Alexandra Poon and is {""}
-        <a href="https://github.com/apoon106/react-weather-app" target="_blank">
+        <a
+          href="https://github.com/apoon106/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           open-sourced on Github
         </a>
       </footer>
