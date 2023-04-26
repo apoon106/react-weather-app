bace68ce05b9e0127798a01414f7e0fef2c30301
apoon106 <121922430+apoon106@users.noreply.github.com>
2023-04-25 21:23:41 -0400

    Removed spacing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App{" "}
+      Weather App
       <footer>
         This project was coded by Alexandra Poon and is {""}
         <a href="https://github.com/apoon106/react-weather-app" target="_blank">
