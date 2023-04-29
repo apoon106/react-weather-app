23ee4ea72291ad3e87ec3419e415eaba6266a5d0
apoon106 <121922430+apoon106@users.noreply.github.com>
2023-04-28 22:44:57 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Alexandra Poon and is {""}
-        <a
-          href="https://github.com/apoon106/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Alexandra Poon and is {""}
+          <a
+            href="https://github.com/apoon106/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
