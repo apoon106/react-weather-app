26ac49cbfb8cf208ebdc3ac2f4c352052741787e
apoon106 <121922430+apoon106@users.noreply.github.com>
2023-05-01 02:22:33 -0400

    Used the correct icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/rain_light.png",
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
@@ -45,6 +45,7 @@ export default function Weather(props) {
                 type="search"
                 placeholder="Enter city"
                 className="form-control"
+                autoFocus="on"
                 onChange={handleCityChange}
               />
             </div>
@@ -53,7 +54,6 @@ export default function Weather(props) {
                 type="submit"
                 value="Search"
                 className="btn btn-primary w-100"
-                autoFocus="on"
               />
             </div>
           </div>
