f2f9adb1271756f23a8462b7e4ce27a0d034d42c
apoon106 <121922430+apoon106@users.noreply.github.com>
2023-05-01 21:37:03 -0400

    Added new component to display relative temperature

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -15,11 +16,12 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="d-flex">
-            <WeatherIcon code={props.data.icon} />
-            <span className="temperature">
-              {Math.round(props.data.temperature)}
-            </span>
-            <span className="unit">°F</span>
+            <div>
+              <WeatherIcon code={props.data.icon} />
+            </div>
+            <div>
+              <WeatherTemperature fahrenheit={props.data.temperature} />
+            </div>
           </div>
         </div>
         <div className="col-6">
